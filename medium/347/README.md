5be0b1e33ab7c523b2084dca4248ddb26df6ac74
Keyv Chan <keyvchan@gmail.com>
2022-04-12 20:17:50 +0800

    347: done

---
+++ medium/347/README.md
@@ -0,0 +1,6 @@
+# Result
+
+```
+Runtime: 16 ms, faster than 55.66% of Go online submissions for Top K Frequent Elements.
+Memory Usage: 5.5 MB, less than 58.98% of Go online submissions for Top K Frequent Elements.
+```
---
+++ medium/347/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println(topKFrequent([]int{1, 1, 1, 2, 2, 3}, 2))
+	fmt.Println(topKFrequent([]int{1}, 1))
+	fmt.Println(topKFrequent([]int{-1, -1}, 1))
+	fmt.Println(topKFrequent([]int{3, 0, 3, 0}, 1))
+}
+
+func topKFrequent(nums []int, k int) []int {
+	count_map := make(map[int]int)
+
+	for _, v := range nums {
+
+		if _, ok := count_map[v]; ok {
+			count_map[v] = count_map[v] + 1
+		} else {
+			count_map[v] = 1
+		}
+	}
+
+	var vals []int
+	for _, val := range count_map {
+		vals = append(vals, val)
+	}
+	sort.Sort(sort.IntSlice(vals))
+	vals = vals[len(vals)-k:]
+
+	var results []int
+	for kk, v := range count_map {
+		if len(results) >= k {
+			break
+		}
+		for _, v1 := range vals {
+			if v == v1 {
+				results = append(results, kk)
+				break
+			}
+		}
+	}
+
+	return results
+}
