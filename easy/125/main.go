33346a690e8893e70f8252e351a5cf1db25733c6
Keyv Chan <keyvchan@gmail.com>
2022-05-19 17:05:28 +0800

    125: done

--- easy/125/main.go
+++ easy/125/main.go
@@ -10,22 +10,34 @@ func main() {
 	fmt.Println(isPalindrome("0P"))
 }
 
+func isAlphanumeric(s byte) bool {
+	if s >= 'a' && s <= 'z' || s >= '0' && s <= '9' {
+		return true
+	}
+	return false
+}
+
 func isPalindrome(s string) bool {
 
 	// remove all non-alphanumeric characters
 	s = strings.ToLower(s)
-	new_string := ""
-	for i := 0; i < len(s); i++ {
-		if s[i] >= 'a' && s[i] <= 'z' || s[i] >= '0' && s[i] <= '9' {
-			// append to new string
-			new_string += string(s[i])
+
+	for i, j := 0, len(s)-1; i < j; {
+		// check s[i] is alphanumeric
+		if !isAlphanumeric(s[i]) {
+			i += 1
+			continue
+		}
+		if !isAlphanumeric(s[j]) {
+			j -= 1
+			continue
 		}
 
-	}
-
-	for i, j := 0, len(new_string)-1; i < j; i, j = i+1, j-1 {
-		if new_string[i] != new_string[j] {
+		if s[i] != s[j] {
 			return false
+		} else {
+			i += 1
+			j -= 1
 		}
 	}
 
