fbcbaa3c01958f8ae6b911ab6b388d0af8d6a1fb
Keyv Chan <keyvchan@gmail.com>
2022-05-24 10:55:51 +0800

    153: done

--- medium/153/README.md
+++
@@ -1,8 +0,0 @@
-# Result
-
-## sort stable
-
-```
-Runtime: 5 ms, faster than 25.66% of Go online submissions for Find Minimum in Rotated Sorted Array.
-Memory Usage: 2.5 MB, less than 68.69% of Go online submissions for Find Minimum in Rotated Sorted Array.
-```
--- medium/153/main.go
+++ medium/153/main.go
@@ -1,19 +1,33 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
+import "fmt"
 
 func main() {
 	fmt.Println(findMin([]int{3, 4, 5, 1, 2}))
-	// fmt.Println(findMin([]int{4, 5, 6, 7, 0, 1, 2}))
 }
 
 func findMin(nums []int) int {
-	sort.SliceStable(nums, func(i, j int) bool {
-		return nums[i] < nums[j]
-	})
 
-	return nums[0]
+	min := 0
+	max := len(nums) - 1
+	mid := 0
+	for {
+		if min == max || max-min == 1 {
+			// check max and min
+			if nums[max] < nums[min] {
+				return nums[max]
+			} else {
+				return nums[min]
+			}
+		}
+		mid = (min + max) / 2
+
+		if nums[mid] > nums[len(nums)-1] {
+			min = mid
+		} else {
+			max = mid
+		}
+
+	}
+
 }
