8ae7346a83f3776be4fda911d4fe35aa5d2530f0
Keyv Chan <keyvchan@gmail.com>
2022-05-31 09:14:13 +0800

    53: done

--- easy/53/main.go
+++ easy/53/main.go
@@ -9,8 +9,15 @@ func main() {
 func maxSubArray(nums []int) int {
 
 	sum := 0
-	for i, v := range nums {
-
+	max := nums[0]
+	for _, v := range nums {
+		if sum < 0 {
+			sum = 0
+		}
+		sum += v
+		if sum > max {
+			max = sum
+		}
 	}
-	return sum
+	return max
 }
