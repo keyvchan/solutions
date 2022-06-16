ec1ddf5bdb1f5dd5f816f85bd021ff3f74571fcc
Keyv Chan <keyvchan@gmail.com>
2022-06-16 12:34:05 +0800

    5: update

--- medium/5/two-pointers.go
+++ medium/5/two-pointers.go
@@ -9,30 +9,34 @@ func main() {
 }
 
 func longestPalindrome(s string) string {
-	length := 0
-	sub := ""
-	for i := 0; i < len(s); i++ {
-		left, right := i, i
-		for left >= 0 && right < len(s) && s[left] == s[right] {
-			// update length
-			if right-left+1 > length {
-				length = right - left + 1
-				sub = s[left : right+1]
+	get_palindrome := func(s string, left, right int, length *int, sub *string) {
+		for {
+			if left < 0 || right >= len(s) || s[left] != s[right] {
+				if *length < right-left {
+					*length = right - left
+					*sub = s[left+1 : right]
+				}
+				break
+			}
+			if s[left] == s[right] {
+				// update length
+				if right-left+1 > *length {
+					*length = right - left + 1
+				}
 			}
 			left--
 			right++
 		}
 
+	}
+	length := 0
+	sub := ""
+	for i := 0; i < len(s); i++ {
+		left, right := i, i
+		get_palindrome(s, left, right, &length, &sub)
+
 		left, right = i, i+1
-		for left >= 0 && right < len(s) && s[left] == s[right] {
-			// update length
-			if right-left+1 > length {
-				length = right - left + 1
-				sub = s[left : right+1]
-			}
-			left--
-			right++
-		}
+		get_palindrome(s, left, right, &length, &sub)
 
 	}
 	return sub
