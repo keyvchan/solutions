45ddcde4ed16df2c3c4ea5e8e06dfd9becf2226e
Keyv Chan <keyvchan@gmail.com>
2022-06-24 21:41:20 +0800

    981: done

--- medium/981/main.go
+++ medium/981/main.go
@@ -1,50 +1,52 @@
 package main
 
-import (
-	"sort"
-)
+import "fmt"
 
 func main() {
 }
 
+type TimeStamp struct {
+	timestamp int
+	value     [2]string
+}
+
 type TimeMap struct {
-	time_index    []int
-	time_to_index map[int]int
-	// this should be a ordered map
-	value_store []map[string]string
+	time []TimeStamp
 }
 
 func Constructor() TimeMap {
-	return TimeMap{
-		value_store:   []map[string]string{},
-		time_to_index: map[int]int{},
-		time_index:    []int{},
-	}
+	return TimeMap{}
 }
 
 func (this *TimeMap) Set(key string, value string, timestamp int) {
-	// time_index store all index of timestamp
-	this.time_index = append(this.time_index, timestamp)
-	this.value_store = append(this.value_store, map[string]string{key: value})
-
-	// time_to_index store the index of corresponding timestamp in value_store
-	this.time_to_index[len(this.time_index)-1] = len(this.value_store) - 1
+	// set key to value at timestamp
+	this.time = append(this.time, TimeStamp{timestamp: timestamp, value: [2]string{key, value}})
 }
 
 func (this *TimeMap) Get(key string, timestamp int) string {
+	// find the largest timestamp smaller than or equal to timestamp
 
-	// find the index of timestamp in time_index
-	index := sort.SearchInts(this.time_index, timestamp)
-	if index == len(this.time_index) {
-		index = len(this.time_index) - 1
-	} else {
-		if this.time_index[index] != timestamp {
-			if index > 0 {
-				index -= 1
+	left, right := 0, len(this.time)-1
+	for {
+		fmt.Println(left, right)
+		if left > right {
+			// we could not find the key, return largest value
+			// find back
+			for i := left; i >= 0; i-- {
+				if this.time[i].value[0] == key {
+					return this.time[i].value[1]
+				}
 			}
+			return ""
 		}
-
+		// check left and right
+		mid := left + (right-left)/2
+		if this.time[mid].timestamp > timestamp {
+			right = mid - 1
+		} else if this.time[mid].timestamp < timestamp {
+			left = mid + 1
+		} else {
+			return this.time[mid].value[1]
+		}
 	}
-
-	return this.value_store[this.time_to_index[index]][key]
 }
