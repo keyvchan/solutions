9e7520bcff330fd7bf35bf38ae1f396f97a521a2
Keyv Chan <keyvchan@gmail.com>
2022-08-19 00:27:01 +0800

    98: done

--- medium/98/main.go
+++ medium/98/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 func main() {
 
 }
@@ -10,39 +12,18 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func dfs(root *TreeNode, parent int, max int, min int, side string) bool {
-	if side == "left" {
-		// check if root.Val is less than min
-		if root.Val >= max {
-			return false
-		}
-		if root.Val >= parent {
-			return false
-		}
-	}
-	if side == "right" {
-		// check if root.Val is greater than max
-		if root.Val <= min {
-			return false
-		}
-		if root.Val <= parent {
-			return false
-		}
-	}
+func dfs(root *TreeNode, min, max int) bool {
 	if root == nil {
 		return true
 	}
-	// set max and min
-	if root.Val > max {
-		max = root.Val
+	// we limit the max and min
+	if root.Val <= min || root.Val >= max {
+		return false
 	}
-	if root.Val < min {
-		min = root.Val
-	}
-	return dfs(root.Left, root.Val, max, min, "left") && dfs(root.Right, root.Val, max, min, "right")
 
+	return dfs(root.Left, min, root.Val) && dfs(root.Right, root.Val, max)
 }
 
 func isValidBST(root *TreeNode) bool {
-	return dfs(root, root.Val, root.Val, root.Val, "root")
+	return dfs(root.Left, -math.MaxInt, math.MaxInt)
 }
