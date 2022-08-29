5ae6756643a77646c783e4b079c79c0e20f48fb6
Keyv Chan <keyvchan@gmail.com>
2022-08-29 15:59:05 +0800

    343: done

--- medium/343/main.go
+++ medium/343/main.go
@@ -1,9 +1,43 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println(integerBreak(2))
+	fmt.Println(integerBreak(10))
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func dfs(n int, memo map[int]int) int {
+	if n == 1 {
+		return 1
+	}
+	if val, ok := memo[n]; ok {
+		return val
+	}
+
+	result := 0
+	for i := 1; i < n; i++ {
+		result = max(result, dfs(i, memo)*dfs(n-i, memo))
+		result = max(result, dfs(i, memo)*(n-i))
+		result = max(result, i*dfs(n-i, memo))
+		result = max(result, i*(n-i))
+	}
+	memo[n] = result
+
+	return result
 
 }
 
 func integerBreak(n int) int {
+	memo := map[int]int{}
+
+	return dfs(n, memo)
 
 }
