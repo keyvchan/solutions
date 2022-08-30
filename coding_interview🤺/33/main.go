5c5b41d78881eb1762d6c57d33bf89781b3f92e9
Keyv Chan <keyvchan@gmail.com>
2022-08-30 13:08:57 +0800

    🤺: update

--- coding_interview🤺/33/main.go
+++ coding_interview🤺/33/main.go
@@ -1,7 +1,10 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(verifyPostorder([]int{1, 6, 3, 2, 5}))
+	fmt.Println(verifyPostorder([]int{1, 3, 2, 6, 5}))
 }
 
 func verifyPostorder(postorder []int) bool {
--- coding_interview🤺/57/II/main.go
+++ coding_interview🤺/57/II/main.go
@@ -1,26 +1,49 @@
 package main
 
 import (
-	"container/list"
 	"fmt"
 )
 
 func main() {
 	fmt.Println(findContinuousSequence(9))
+	fmt.Println(findContinuousSequence(15))
+}
+
+func sum(i, j int) int {
+	// sum of range i to j
+	return j*(j+1)/2 - (i-1)*(i)/2
 }
 
 func findContinuousSequence(target int) [][]int {
 
-	// find the first sequence that sums >= target
+	result := [][]int{}
 
-	sum := 0
-	sequence := list.New()
-	for i := 0; i <= target; i++ {
-		sum += i
-		if sum > target {
-			break
+	// start from 1 to target
+	i, j := 1, 1
+
+	for i < target && j < target {
+
+		// check if sum of range i to j is equal to target
+		if sum(i, j) == target {
+			temp := []int{}
+			for k := i; k <= j; k++ {
+				temp = append(temp, k)
+			}
+			result = append(result, temp)
+			i++
+			continue
 		}
-		sequence.PushBack(i)
+		if sum(i, j) > target {
+			i++
+			continue
+		}
+		if sum(i, j) < target {
+			j++
+			continue
+		}
+
 	}
+
+	return result
 
 }
