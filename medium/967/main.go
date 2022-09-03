a0a5ee1f8d42b40336eecef8db11d1f4413ec010
Keyv Chan <keyvchan@gmail.com>
2022-09-03 15:16:45 +0800

    967: done

--- medium/967/main.go
+++ medium/967/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
@@ -10,38 +9,33 @@ func main() {
 	fmt.Println(numsSameConsecDiff(2, 1))
 }
 
-func backtracking(current string, n int, k int, result *[]string) {
+func backtracking(current int, last int, n int, k int, result *[]int) {
 	if n == 0 {
 		*result = append(*result, current)
 		return
 	}
 
-	// check last digit
-	lastDigit, _ := strconv.Atoi(string(current[len(current)-1]))
 	// if lastDigit - k < 0 && lastDigit + k > 9, impossible to make a number
-	if lastDigit-k >= 0 {
-		backtracking(current+strconv.Itoa(lastDigit-k), n-1, k, result)
+	if last-k >= 0 {
+
+		temp := current*10 + last - k
+		backtracking(temp, last-k, n-1, k, result)
 	}
 	// if lastDigit-k == lastDigit+k, it will be duplicated
-	if lastDigit-k != lastDigit+k && lastDigit+k <= 9 {
-		backtracking(current+strconv.Itoa(lastDigit+k), n-1, k, result)
+	if last-k != last+k && last+k <= 9 {
+		temp := current*10 + last + k
+		backtracking(temp, last+k, n-1, k, result)
 	}
 }
 
 func numsSameConsecDiff(n int, k int) []int {
 	// use string to store the number
 
-	result := []string{}
+	result := []int{}
 	// the first digit is not zero
 	for i := 1; i <= 9; i++ {
-		backtracking(strconv.Itoa(i), n-1, k, &result)
+		backtracking(i, i, n-1, k, &result)
 	}
 
-	// convert string to int
-	intResult := []int{}
-	for _, v := range result {
-		temp_v, _ := strconv.Atoi(v)
-		intResult = append(intResult, temp_v)
-	}
-	return intResult
+	return result
 }
